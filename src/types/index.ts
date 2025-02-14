dc96ddbde5f7494e590c0f5c8d048fb44256022d
mxber2022 <mxber2022@gmail.com>
2025-02-14 15:16:57 +0100

    vite project init

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface FeatureCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+export interface StatCardProps {
+  number: string;
+  label: string;
+}
