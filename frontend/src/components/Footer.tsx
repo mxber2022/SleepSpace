5e8a9962966bb3c906593b2671e700ab18f6f438
mxber2022 <mxber2022@gmail.com>
2025-02-20 19:56:14 +0100

    X twitter

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,6 +8,20 @@ const SleepSpaceLogo = () => (
   </svg>
 );
 
+const TwitterIcon = (props:any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    enableBackground="new 0 0 72 72"
+    viewBox="0 0 72 72"
+    id="twitter-x"
+    {...props} // Allow passing className and other props
+  >
+    <g>
+      <path d="M42.5,31.2L66,6h-6L39.8,27.6L24,6H4l24.6,33.6L4,66h6l21.3-22.8L48,66h20L42.5,31.2z M12.9,10h8l38.1,52h-8L12.9,10z" />
+    </g>
+  </svg>
+);
+
 export function Footer() {
   return (
     <footer className="relative mt-32">
@@ -40,7 +54,7 @@ export function Footer() {
           </p>
 
           <div className="flex gap-6">
-            <SocialLink href="https://x.com/Sleepspace4U" icon={<Twitter className="w-5 h-5" />} label="Twitter" />
+            <SocialLink href="https://x.com/Sleepspace4U" icon={<TwitterIcon className="w-5 h-5" />} label="Twitter" />
             <SocialLink href="https://github.com/mxber2022/SleepSpace" icon={<Github className="w-5 h-5" />} label="GitHub" />
             <SocialLink href="#linkedin" icon={<Linkedin className="w-5 h-5" />} label="LinkedIn" />
           </div>
