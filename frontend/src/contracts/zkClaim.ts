7a049e7ee0ce3f5c072acffdd384c1beea56ef2e
mxber2022 <mxber2022@gmail.com>
2025-02-21 17:49:28 +0100

    Create zkClaim.ts

---
+++ frontend/src/contracts/zkClaim.ts
@@ -0,0 +1,5 @@
+export const SLEEP_GOALS_ABI = [
+    "function verifyProof(Reclaim.Proof memory proof) public view"
+]
+
+export const ZKClaim_ADDRESS = "0x5A7413e61a265B9adB77C88FD02cc10464820110";
