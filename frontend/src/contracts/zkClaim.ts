c777afde4bb2bf9450aa7d03de92ecccbbf8e379
mxber2022 <mxber2022@gmail.com>
2025-02-21 19:48:03 +0100

    Token claim verification - working

--- frontend/src/contracts/zkClaim.ts
+++ frontend/src/contracts/zkClaim.ts
@@ -1,5 +1,23 @@
-export const SLEEP_GOALS_ABI = [
-    "function verifyProof(Reclaim.Proof memory proof) public view"
-]
+export const verifyProof_ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "bytes",
+          "name": "proof",
+          "type": "bytes"
+        }
+      ],
+      "name": "verifyProof",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
 
-export const ZKClaim_ADDRESS = "0x5A7413e61a265B9adB77C88FD02cc10464820110";
+export const ZKClaim_ADDRESS = "0x5A7413e61a265B9adB77C88FD02cc10464820110"; 
--- frontend/src/hooks/useZKClaim.ts
+++ frontend/src/hooks/useZKClaim.ts
@@ -1,7 +1,28 @@
 import { useState, useCallback } from 'react';
 import { BrowserProvider, Eip1193Provider, ethers } from 'ethers';
-import { SLEEP_GOALS_ABI, ZKClaim_ADDRESS } from '../contracts/zkClaim';
+import { verifyProof_ABI, ZKClaim_ADDRESS } from '../contracts/zkClaim';
 import { useAppKitAccount, useAppKitProvider } from '@reown/appkit/react';
+import { abi } from './proofABI';
+
+const tempProof= {
+  claimInfo: {
+    context: '{"extractedParameters":{"price":"2670.7"},"providerHash":"0x5dbe58ad866070178af5c86d8caac49014934f9499a5ebd9599961325bfc347d"}',
+    parameters: '{"body":"","method":"GET","responseMatches":[{"type":"regex","value":"\\\\{\\"ethereum\\":\\\\{\\"usd\\":(?<price>[\\\\d\\\\.]+)\\\\}\\\\}"}],"responseRedactions":[],"url":"https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"}',
+    provider: 'http'
+  },
+  signedClaim: {
+    claim: {
+      epoch: 1,
+      identifier: '0x00e0c79f22b796b31c56b53febfc2e87d126f9dd4c0f7b6f6b9e11beef499208',
+      owner: '0x381994d6b9b08c3e7cfe3a4cd544c85101b8f201',
+      timestampS: 1740161672
+    },
+    signatures: [
+      '0xfe323d2fca8b9d777a39bd44de9bebb2e0ad0e1cf4a95428eb48d4f4def67baf0429ca999467aa7804c583c8a39163dd1439f491c247b74f544539dd5c80be9b1b'
+    ]
+  }
+}
+
 
 export function useZKClaim() {
   const { address, isConnected } = useAppKitAccount();
@@ -20,16 +41,17 @@ export function useZKClaim() {
     try {
       const provider = new BrowserProvider(walletProvider as Eip1193Provider);
       const signer = await provider.getSigner();
-      const contract = new ethers.Contract(ZKClaim_ADDRESS, SLEEP_GOALS_ABI, signer);
+      console.log("verifyProof_ABI: ",verifyProof_ABI);
+      const contract = new ethers.Contract(ZKClaim_ADDRESS, abi, signer);
 
       // Call verifyProof function on contract with proof
-      const tx = await contract.verifyProof(proof);
+      const tx = await contract.verifyProof((tempProof));
       await tx.wait();
 
       return true;
     } catch (err: any) {
-      console.error('Error minting NFT:', err);
-      setError(err.reason || 'Failed to mint NFT');
+      console.error('Error verifying proof:', err);
+      setError(err.reason || 'Failed to verifying proof');
       return false;
     } finally {
       setIsLoading(false);
