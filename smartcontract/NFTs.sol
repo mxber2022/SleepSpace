dfb41a7a24bb5a65a8242eb7ac13f915eaf145b4
mxber2022 <mxber2022@gmail.com>
2025-02-22 16:41:20 +0100

    upgradable uri - onlyOwner

--- smartcontract/NFTs.sol
+++ smartcontract/NFTs.sol
@@ -14,6 +14,17 @@ contract ZZZ is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Owna
 
     constructor(address initialOwner) ERC721("ZZZ", "ZZZ") Ownable(initialOwner) {}
 
+    string[7] private _predefinedURIs = [
+        "ipfs://QmURI1", // 1. Dream Weaver
+        "ipfs://QmURI2", // 2. Circadian Tuner
+        "ipfs://QmURI3", // 3. Restful Pillow
+        "ipfs://QmURI4", // 4. Lucid Dream Catcher
+        "ipfs://QmURI5", // 5. Sleep Guardian
+        "ipfs://QmURI6", // 6. Melatonin Crystal
+        "ipfs://QmURI7"  // 7. Sleep Accelerator
+    ];
+
+
     function pause() public onlyOwner {
         _pause();
     }
@@ -25,12 +36,21 @@ contract ZZZ is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Owna
     function safeMint(address to, string memory uri) public {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _setTokenURI(tokenId, _predefinedURIs[0]);
     }
 
-    /* Upgrade NFT Based on sleep data */
-    function upgradeURI(uint256 tokenId, string memory uri) public {
+    /* Upgrade NFT Based on sleep data. possible integratuíon zk */
+    function upgradeURI(uint256 tokenId, string memory uri) public onlyOwner{
         require(ownerOf(tokenId) == msg.sender, "You do not own this NFT");
+
+        // 1. Dream Weaver - first nft minted (ignore)
+        // 2. Circadian Tuner - 5% bonus when hitting targeted weekly sleep performance
+        // 3. Restful Pillow - 10% bonus Achieve 8 hours of sleep for 3 consecutive nights
+        // 4. Lucid Dream Catcher - 15 % bonus Achieve optimal sleep quality with rem cycle
+        // 5. Sleep Guardian - 2x earning Maintain 90%+ sleep score for 30 consecutive days
+        // 6. Melatonin Crystal - 3x earning 8 hours of high-quality sleep for 30 consecutive days
+        // 7. Sleep Accelerator - Consistently achieve high sleep scores over time of 3 months
+
         _setTokenURI(tokenId, uri);
     }
 
