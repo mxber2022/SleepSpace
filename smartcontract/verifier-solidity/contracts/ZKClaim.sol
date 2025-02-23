73dbf62aa44a5916436149d7b07a0afdcf9724f6
mxber2022 <mxber2022@gmail.com>
2025-02-23 06:51:40 +0100

    withdraw token update

--- smartcontract/verifier-solidity/contracts/ZKClaim.sol
+++ smartcontract/verifier-solidity/contracts/ZKClaim.sol
@@ -2,18 +2,30 @@
 pragma solidity 0.8.4;
 
 import "./Reclaim.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ZKClaim {
    address public reclaimAddress;
+   IERC20 private _rewardToken = IERC20(0xb507eEF94DB8AFa44BE58Fb687c5234FddF14b44); //sleep token
+   
+   event TokenClaimed(address indexed user, uint256 amount);
 
    constructor() {
       // Replace with the network you are deploying on
       reclaimAddress = 0xAe94FB09711e1c6B057853a515483792d8e474d0; 
    }
 
-   function verifyProof(Reclaim.Proof memory proof) public view {
+   function verifyProof(Reclaim.Proof memory proof,  uint256 amount) public {
        Reclaim(reclaimAddress).verifyProof(proof);
        // Your business logic upon successful verification
        // Example: Verify that proof.context matches your expectations
+       withdrawTokens(amount);
    }
-}
+
+   function withdrawTokens(uint256 amount) internal {
+      require(_rewardToken.balanceOf(address(this)) >= amount, "Insufficient contract balance");
+      require(_rewardToken.transfer(msg.sender, amount), "Transfer failed");
+
+      emit TokenClaimed(msg.sender, amount);
+   }
+} 
